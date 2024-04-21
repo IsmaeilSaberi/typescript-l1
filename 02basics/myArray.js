699a6e03a6e3f62dd12be233c60d9751c8a00933
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-04-20 20:43:06 -0700

    arrays and unions

---
+++ 02basics/myArray.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [[255, 255, 255], []];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
---
+++ 02basics/myUnion.ts
@@ -0,0 +1,30 @@
+let score: number | string = 33;
+
+score = 44;
+
+score = "55";
+
+type User = { name: string; id: number };
+type Admin = { username: string; id: number };
+
+let ismaeil: User | Admin = { name: "ismaeil", id: 334 };
+
+ismaeil = { username: "isameilsaberi", id: 333 };
+
+// function getDbId(id: number | string) {
+//   // making some API calls
+//   console.log(`DB id is ${id}`);
+// }
+
+getDbId(3);
+getDbId("3");
+
+function getDbId(id: number | string) {
+  if (typeof id === "string") id.toLowerCase();
+}
+
+// array
+
+const data: number[] = [1, 2, 3];
+const data2: string[] = ["1", "2", "3"];
+const data3: (string | number)[] = ["1", "2", "3", 4];
