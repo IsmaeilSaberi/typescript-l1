5590c728983ddeedb73aca5eeed34c7c6db24c82
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-04-23 00:06:40 -0700

    class concepts in type script

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("type script is now here!");
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name // private userId: string
+    ) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "";
+    }
+    deleteToken() {
+        console.log("token deleted successfully");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const ismaeil = new User("i@i.com", "ismaeil");
+// ismaeil.name;
+// ismaeil.deleteToken();
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,1 +1,50 @@
-console.log("type script is now here!");
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+class User {
+  protected _courseCount = 1;
+
+  readonly city: string = "";
+  constructor(
+    public email: string,
+    public name: string // private userId: string
+  ) {}
+
+  private deleteToken() {
+    console.log("token deleted successfully");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
+}
+
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
+}
+
+const ismaeil = new User("i@i.com", "ismaeil");
+// ismaeil.name;
+
+// ismaeil.deleteToken();
